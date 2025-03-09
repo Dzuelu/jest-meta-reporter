56d21cc3ba2f796ab4f2c4efad2a97ac530eedbf
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-03-09 17:30:30 -0400

    output just meta object for fn

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-confusing-void-expression, @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { Test, TestCaseResult } from '@jest/test-result';
+// import { Test, TestCaseResult } from '@jest/test-result';
+import type { Config, Test, TestCaseResult } from '@jest/reporters';
 import { state } from 'jest-metadata';
 // eslint-disable-next-line import/no-named-as-default
 import JestMetadataReporter from 'jest-metadata/reporter';
@@ -18,6 +19,11 @@ export interface MetaReporterParams {
 }
 
 export class MetaReporter extends JestMetadataReporter {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(globalConfig: Config.GlobalConfig) {
+    super(globalConfig);
+  }
+
   override async onTestCaseResult(test: Test, testCaseResult: TestCaseResult) {
     await super.onTestCaseResult(test, testCaseResult);
     // if (testCaseResult.status !== 'failed') return;
@@ -31,26 +37,10 @@ export class MetaReporter extends JestMetadataReporter {
     const allTestInvocations = Array.from(fileMetadata.allTestInvocations());
     const invocation = allTestInvocations.find(i => i.id === invocationId);
     if (invocation?.fn == null) return;
-    const parsed = parseId(invocationId);
-    const meta = [pluginSpace, ...parsed.split('.')].reduce<Readonly<Data>>((obj, key) => {
-      if (!Object.hasOwn(obj, key)) console.log('TypeError incoming...', { invocationId, key, obj, parsed });
+    const meta = [pluginSpace, ...parseId(invocationId).split('.')].reduce<Readonly<Data>>((obj, key) => {
       return obj[key] as Readonly<Data>;
     }, invocation.fn.get());
 
-    console.log(`onTestCaseResult`, {
-      meta,
-      // // allTestInvocationsGet: allTestInvocations.map(i => JSON.stringify(i.get())),
-      // allTestInvocationsIds: allTestInvocations.map(i => i.id),
-      // // test,
-      // // allTestInvocationsDefinition: allTestInvocations.map(i => i.definition),
-      // // invocation,
-      // meta,
-      // metaId: invocationId,
-      // // testCaseResult,
-      // title: testCaseResult.title
-      // // firstMeta: JSON.stringify(Array.from(fileMeta.allTestInvocations())[0].fn?.get(), undefined, 2),
-      // // secondMeta: JSON.stringify(Array.from(fileMeta.allTestInvocations())[1]?.fn?.get() ?? {}, undefined, 2)
-      rawMeta: invocation.fn.get()
-    });
+    console.log(meta);
   }
 }

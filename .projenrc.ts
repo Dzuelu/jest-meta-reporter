4af0dc08dbc05a9ef60ae48a21e984e3095ce777
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-03-10 23:10:35 -0400

    cleanup some projen config

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,12 +7,6 @@ const project = new DzueluTypeScriptProject({
     devdirs: ['test', 'e2e'],
     dirs: ['src']
   },
-  jestOptions: {
-    // jestConfig: {
-    //   testEnvironment: 'jest-metadata/environment-node'
-    // },
-    // preserveDefaultReporters: false
-  },
   name: 'jest-meta-reporter',
   releaseToNpm: true
 });
@@ -23,13 +17,10 @@ project.addPeerDeps('jest');
 
 // compile the reporter so we can use it in this project
 project.testTask.prependExec('projen compile');
-project.testTask.prependExec('rm -rf ./dist', { condition: 'node -e "if (process.env.GITHUB_ENV) process.exit(1)"' });
-
-// TODO: something like this for e2e tests
-// const e2e = project.addTask('e2e', { exec: 'jest --testMatch "<rootDir>/@(e2e)/**/*(*.)@(spec|test).ts?(x)"' });
-// project.testTask.spawn(e2e);
-
-// project.jest?.addReporter(new JestReporter('./dist/src/index.js'));
+project.testTask.prependExec('rm -rf ./dist', {
+  // Don't clean dist folder when in pipeline (was already clean)
+  condition: 'node -e "if (process.env.GITHUB_ENV) process.exit(1)"'
+});
 
 project.tsconfig?.addExclude('e2e/**');
 
